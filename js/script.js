840f9163b88b453d45ad5e0902f93079f8aa498c
Danny <11011967+dc820@users.noreply.github.com>
2019-01-23 20:48:35 -0500

    Add BTN in modal expands table. Fixing checkboxes for deleting rows

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
-    // Handler for .ready() called.
-    console.log("doc is ready");
+  console.log("doc is ready");
+
+  //VARIABLES
+  var rowCount = 0;
+  var r;
+
     $("nav > ul > li > a").click(function () {
         $(".active").removeClass("active");
         $(this).addClass("active");
@@ -27,7 +31,45 @@ $(document).ready(function () {
         }
     });
 
-    $("#AddItem").click(function(){
-        $("#AddItemModal").modal();
+    //Code for Add to Table Button Here
+    $("#AddToTable").click(function(){
+        $("#AddModal").modal();
     });
+
+    $("#ModalAddButton").click(function(){
+        rowCount++;
+        console.log("row count " + rowCount);
+        $(".addNewRow").append(
+            '<tr id="tableRow'+rowCount+'"><td><div class="custom-control custom-checkbox"><input type="checkbox" class="custom-control-input" id="CheckRow' + rowCount + '"><label class="custom-control-label" for="CheckRow'+ rowCount +'">Cisco Router</label></div></td><td>10x</td><td>Cisco Router- 10X</td><td>45</td><td>143</td><td>76</td><td>86</td><td>88</td></tr>'
+        );
+    });
+
+    //Code for Edit Button Here
+    $("#Edit").click(function(){
+        $("#EditModal").modal();
+    });
+
+    //Code for Refresh Button Here
+    $("#Delete").click(function(){
+        $("#DeleteModal").modal();
+    });
+    
+    $("#ConfirmDelete").click(function(){
+        checkSelection();
+        console.log($(".selected"));
+        $(".selected").remove();
+    });
+
+    
+    function checkSelection() {     //Something wrong with selection. First checkbox selects all html table rows
+        for(r=1; r<=rowCount; r++){
+            if($("input[type=checkbox]").prop('checked') == true){
+                $("#tableRow"+r).addClass("selected");
+            } else if ($("input[type=checkbox]").prop('checked') == false){ 
+                $("#tableRow"+r).removeClass("selected") 
+            } else {
+                return;
+            }
+        }
+    }
 });
