df89b3531d5f6cb0f1c8e2c441ee3f12933cc4f8
Danny <11011967+dc820@users.noreply.github.com>
2019-02-20 13:13:44 -0500

    Added to Main Component. Can Push to Row Array to Populate Table

--- angular-inventory/src/app/app.module.ts
+++ angular-inventory/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { AddModalComponent } from './main/add-modal/add-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    AddModalComponent
   ],
   imports: [
     BrowserModule
--- angular-inventory/src/app/main/main.component.ts
+++ angular-inventory/src/app/main/main.component.ts
@@ -6,6 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  // Heading for each cell can be modified here
+  headings = ['Device', 'Model', 'Label', 'Starting', 'Recieved', 'Shipped', 'On-hand', 'Minimum' ];
+
+  // Dummy data. Can push row obects to array to populate inventory table
+  tRow = [
+    { id: '1',
+      checkbox: 'u1',
+      device: 'Router',
+      model: '4300',
+      label: 'Cisco Router- 4300',
+      start: '45',
+      recieved: '143',
+      shipped: '76',
+      onhand: '86',
+      minimum: '88' },
+
+    { id: '2',
+      checkbox: 'u2',
+      device: 'Router',
+      model: '4300',
+      label: 'Cisco Router- 4300',
+      start: '45',
+      recieved: '143',
+      shipped: '76',
+      onhand: '86',
+      minimum: '88' }
+    ];
+
+  // Footer icons on table
+  footings = [ { href: '#', id: 'Add', icon: 'fas fa-plus' },
+             { href: '#', id: 'Edit', icon: 'fas fa-pencil-alt' },
+             { href: '#', id: 'Remove', icon: 'fas fa-trash' },
+             { href: '#', id: 'Resync', icon: 'fas fa-sync-alt' } ];
 
   constructor() { }
 
