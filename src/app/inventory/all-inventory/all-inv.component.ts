d2e8b6d1d329a451028a203e93bb6d83752f7cf0
Dan <dancolon95@gmail.com>
2019-05-31 17:45:13 -0400

    Checkboxes Working Properly. Working On Theming

--- src/app/inventory/all-inventory/all-inv.component.ts
+++ src/app/inventory/all-inventory/all-inv.component.ts
@@ -47,8 +47,8 @@ export class AllInvComponent implements OnInit, OnDestroy {
   selection = new SelectionModel<object>(true, []);
   trafficSelection = new SelectionModel<string>(true, []);
   childSelection = new SelectionModel<object>(true, []);
-  indeterminateRowSelection = new SelectionModel<object>(true, []);
-  indeterminateTrafficSelection = new SelectionModel<object>(true, []);
+  indeterminateSelection = new SelectionModel<object>(true, []);
+  indeterminateTrafficSelection = new SelectionModel<string>(true, []);
   // Pagination & Sort For Table
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -109,8 +109,7 @@ export class AllInvComponent implements OnInit, OnDestroy {
     this.selection.clear();
     this.trafficSelection.clear();
     this.childSelection.clear();
-    this.indeterminateRowSelection.clear();
-    this.indeterminateRowSelection.clear();
+    this.indeterminateSelection.clear();
     this.indeterminateTrafficSelection.clear();
   }
   /**
@@ -188,8 +187,6 @@ export class AllInvComponent implements OnInit, OnDestroy {
    */
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
-    console.log(this.indeterminateRowSelection.selected);
-    console.log(this.indeterminateTrafficSelection.selected);
   }
   /**
    * Whether The Number Of Selected Elements Matches The Total Number Of Rows
@@ -217,7 +214,7 @@ export class AllInvComponent implements OnInit, OnDestroy {
       this.GROUPED_DEVICES.forEach((group: []) => {
         const filteredGroup = group.filter((device: Device) => device.model === deviceGroup.model);
         filteredGroup.forEach((device: Device) => {
-          this.trafficSelection.select(device.traffic);
+          this.trafficSelection.select(device.traffic + deviceGroup.model);
           this.childSelection.select(device);
         });
       });
@@ -225,7 +222,7 @@ export class AllInvComponent implements OnInit, OnDestroy {
       this.GROUPED_DEVICES.forEach((group: []) => {
         const filteredGroup = group.filter((device: Device) => device.model === deviceGroup.model);
         filteredGroup.forEach((device: Device) => {
-          this.trafficSelection.deselect(device.traffic);
+          this.trafficSelection.deselect(device.traffic + deviceGroup.model);
           this.childSelection.deselect(device);
         });
       });
@@ -235,25 +232,112 @@ export class AllInvComponent implements OnInit, OnDestroy {
    * Parent Checkbox To Toggle All Children
    */
   selectChildren(deviceGroup: Device, traffic: string) {
-    if (this.trafficSelection.isSelected(traffic)) {
+    if (this.trafficSelection.isSelected(traffic + deviceGroup.model)) {
       this.GROUPED_DEVICES.forEach((group: []) => {
-        const filteredGroup = group.filter((device: Device) => device.model === deviceGroup.model && device.traffic === traffic);
-        filteredGroup.forEach((device: Device) => {
-          // this.indeterminateTrafficSelection.deselect(deviceGroup);
-          this.trafficSelection.select(traffic);
-          this.childSelection.select(device);
+        const groupDevices = group.filter((filteredModels: Device) => filteredModels.model === deviceGroup.model);
+        const filteredGroup = groupDevices.filter((filteredTraffic: Device) => filteredTraffic.traffic === traffic);
+
+        filteredGroup.forEach((filteredDevice: Device) => {
+          this.trafficSelection.select(traffic + deviceGroup.model);
+          this.childSelection.select(filteredDevice);
+          this.indeterminateSelection.select(deviceGroup);
+        });
+
+        let deviceCount = 0;
+        groupDevices.forEach((networkDevice) => {
+          if (this.childSelection.isSelected(networkDevice)) {
+            deviceCount++;
+          }
+
+          if (deviceCount === groupDevices.length) {
+            this.indeterminateSelection.deselect(deviceGroup);
+            this.selection.select(deviceGroup);
+          }
         });
       });
     } else {
       this.GROUPED_DEVICES.forEach((group: []) => {
-        const filteredGroup = group.filter((device: Device) => device.model === deviceGroup.model && device.traffic === traffic);
-        filteredGroup.forEach((device: Device) => {
-          this.trafficSelection.deselect(device.traffic);
-          this.childSelection.deselect(device);
+        const groupDevices = group.filter((filteredModels: Device) => filteredModels.model === deviceGroup.model);
+        const filteredGroup = groupDevices.filter((filteredTraffic: Device) => filteredTraffic.traffic === traffic);
+        filteredGroup.forEach((filteredDevice: Device) => {
+          this.trafficSelection.deselect(traffic + deviceGroup.model);
+          this.childSelection.deselect(filteredDevice);
+          if (this.indeterminateSelection.isSelected(deviceGroup) && this.selection.selected) {
+              this.indeterminateSelection.deselect(deviceGroup);
+          }
+        });
+
+        let deviceCount = 0;
+        groupDevices.forEach((networkDevice) => {
+          if (this.childSelection.isSelected(networkDevice)) {
+            deviceCount++;
+          }
+
+          if (deviceCount === 0) {
+            this.indeterminateSelection.deselect(deviceGroup);
+          }
+
+          if (deviceCount < groupDevices.length && deviceCount > 0) {
+            this.selection.deselect(deviceGroup);
+            this.indeterminateSelection.select(deviceGroup);
+          }
+
         });
       });
     }
   }
+
+  onChildChecked(device: Device, deviceGroup: Device, traffic: string) {
+    if (device.traffic === traffic && device.model === deviceGroup.model) {
+      this.indeterminateTrafficSelection.select(traffic + deviceGroup.model);
+      this.indeterminateSelection.select(deviceGroup);
+    }
+    const trafficArr = [];
+    this.GROUPED_DEVICES.forEach((group: []) => {
+      const groupDevices = group.filter((filteredModels: Device) => filteredModels.model === deviceGroup.model);
+      const filteredGroup = groupDevices.filter((filteredTraffic: Device) => filteredTraffic.traffic === device.traffic);
+      filteredGroup.forEach((filteredDevice: Device) => {
+        if (this.childSelection.isSelected(filteredDevice)) {
+          trafficArr.push(filteredDevice);
+        }
+
+        if (trafficArr.length === 0) {
+          this.indeterminateTrafficSelection.deselect(traffic + deviceGroup.model);
+          this.trafficSelection.deselect(traffic + deviceGroup.model);
+        }
+
+        if (trafficArr.length > 0 && trafficArr.length < filteredGroup.length) {
+          this.trafficSelection.deselect(traffic + deviceGroup.model);
+          this.indeterminateTrafficSelection.select(traffic + deviceGroup.model);
+        }
+
+        if (trafficArr.length === filteredGroup.length) {
+          this.indeterminateTrafficSelection.deselect(traffic + deviceGroup.model);
+          this.trafficSelection.select(traffic + deviceGroup.model);
+        }
+
+        let deviceCount = 0;
+        groupDevices.forEach((networkDevice) => {
+          if (this.childSelection.isSelected(networkDevice)) {
+            deviceCount++;
+          }
+
+          if (deviceCount === groupDevices.length) {
+            this.indeterminateSelection.deselect(deviceGroup);
+            this.selection.select(deviceGroup);
+          } else {
+            this.indeterminateSelection.select(deviceGroup);
+            this.selection.deselect(deviceGroup);
+          }
+
+          if (deviceCount === 0) {
+            this.indeterminateSelection.deselect(deviceGroup);
+          }
+        });
+      });
+    });
+  }
+
   /**
    * The Label For The Checkbox On The Passed Row
    */
